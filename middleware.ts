9a7ec2eab655de0c3dc1e8b8b00bbbc089a96c7d
Samuel <samwoker112@gmail.com>
2025-10-12 15:02:01 +0300

    first commit

---
+++ middleware.ts
@@ -0,0 +1,36 @@
+import { clerkMiddleware,createRouteMatcher } from '@clerk/nextjs/server';
+
+
+const isPublicRoute = createRouteMatcher([
+    '/',
+    '/events/:id',
+    '/sign-in',
+    '/sign-up',
+])
+const ignoredRoutes = [
+    '/api/webhook/clerk',
+    '/api/webhook/stripe',
+    '/api/uploadthing'
+]
+const isIgnoreRoute =(pathname:string)=>{
+    return ignoredRoutes.some((route)=>pathname.startsWith(route))
+}
+
+export default clerkMiddleware(async (auth,req)=>{
+    const {pathname} = req.nextUrl
+
+    if(isIgnoreRoute(pathname)) return
+
+    if(!isPublicRoute(req)){
+    await auth.protect()
+    }
+});
+
+export const config = {
+  matcher: [
+    // Skip Next.js internals and all static files, unless found in search params
+    '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
+    // Always run for API routes
+    '/(api|trpc)(.*)',
+  ],
+};
