c5ddd1d4f3131334bda6282396bb8fbabb45f8be
Samuel <samwoker112@gmail.com>
2025-10-14 08:49:09 +0300

    fixing header component

--- components/shared/Header.tsx
+++ components/shared/Header.tsx
@@ -14,14 +14,16 @@ const Header = () => {
           alt="Evently Logo"
           />
           </Link>
+          <SignedIn>
             <nav className="md:flex-between hidden w-full max-w-xs">
                 <NavItems/>
             </nav>
+            </SignedIn>
           <div className="flex w-32 items-center gap-3">
       <SignedIn>
         <UserButton />
-      </SignedIn>
        <MobileNav/>
+      </SignedIn>
             <SignedOut>
                 <Button asChild className="rounded-full" size="lg">
                     <Link href="/sign-in">Login
